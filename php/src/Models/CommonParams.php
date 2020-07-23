b8f42dbbd59ea3c74506e14df43ffa822146af49
wb-hx510875 <wb-hx510875@alibaba-inc.com>
2020-07-23 18:29:54 +0800

    PHP Patch 1. add test case 2. format code 3. add readme

--- php/src/Models/CommonParams.php
+++ php/src/Models/CommonParams.php
@@ -1,22 +1,62 @@
 <?php
 
 // This file is auto-generated, don't edit it. Thanks.
+
 namespace AlibabaCloud\SDK\IotApiGateway\Models;
 
 use AlibabaCloud\Tea\Model;
 
-class CommonParams extends Model {
+class CommonParams extends Model
+{
+    /**
+     * @description API版本
+     *
+     * @var string
+     */
+    public $apiVer;
+
+    /**
+     * @description 登录用户的token(客户端)
+     *
+     * @var string
+     */
+    public $iotToken;
+
+    /**
+     * @description 云端资源token（云端)
+     *
+     * @var string
+     */
+    public $cloudToken;
+
+    /**
+     * @description 国际化扩展，语言
+     *
+     * @var string
+     */
+    public $language;
+
+    /**
+     * @description 国际化扩展，地理位置、ip
+     *
+     * @var string
+     */
+    public $locale;
     protected $_name = [
-        'apiVer' => 'apiVer',
-        'iotToken' => 'iotToken',
+        'apiVer'     => 'apiVer',
+        'iotToken'   => 'iotToken',
         'cloudToken' => 'cloudToken',
-        'language' => 'language',
-        'locale' => 'locale',
+        'language'   => 'language',
+        'locale'     => 'locale',
     ];
-    public function validate() {
+
+    public function validate()
+    {
         Model::validateRequired('apiVer', $this->apiVer, true);
     }
-    public function toMap() {
+
+    public function toMap()
+    {
         $res = [];
         if (null !== $this->apiVer) {
             $res['apiVer'] = $this->apiVer;
@@ -33,59 +73,34 @@ class CommonParams extends Model {
         if (null !== $this->locale) {
             $res['locale'] = $this->locale;
         }
+
         return $res;
     }
+
     /**
      * @param array $map
+     *
      * @return CommonParams
      */
-    public static function fromMap($map = []) {
+    public static function fromMap($map = [])
+    {
         $model = new self();
-        if(isset($map['apiVer'])){
+        if (isset($map['apiVer'])) {
             $model->apiVer = $map['apiVer'];
         }
-        if(isset($map['iotToken'])){
+        if (isset($map['iotToken'])) {
             $model->iotToken = $map['iotToken'];
         }
-        if(isset($map['cloudToken'])){
+        if (isset($map['cloudToken'])) {
             $model->cloudToken = $map['cloudToken'];
         }
-        if(isset($map['language'])){
+        if (isset($map['language'])) {
             $model->language = $map['language'];
         }
-        if(isset($map['locale'])){
+        if (isset($map['locale'])) {
             $model->locale = $map['locale'];
         }
+
         return $model;
     }
-    /**
-     * @description API版本
-     * @var string
-     */
-    public $apiVer;
-
-    /**
-     * @description 登录用户的token(客户端)
-     * @var string
-     */
-    public $iotToken;
-
-    /**
-     * @description 云端资源token（云端)
-     * @var string
-     */
-    public $cloudToken;
-
-    /**
-     * @description 国际化扩展，语言
-     * @var string
-     */
-    public $language;
-
-    /**
-     * @description 国际化扩展，地理位置、ip
-     * @var string
-     */
-    public $locale;
-
 }
